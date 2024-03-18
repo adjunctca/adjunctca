e3e649f978057fcf0d3ac2a086920fba73bbaea5
adjunctca <162944407+adjunctca@users.noreply.github.com>
2024-03-18 11:51:17 -0700

    Update windowsupdatereboot.ps1

--- windowsupdatereboot.ps1
+++ windowsupdatereboot.ps1
@@ -17,15 +17,16 @@ function CheckAndInstallUpdates {
   $updateAvailable = Get-WindowsUpdate -Verbose
   if ($updateAvailable) {
     Write-Output "Updates available. Installing..."
-    Get-WindowsUpdate -Install -AcceptAll -Verbose
-    Restart-Computer -Wait -Force
+    Get-WindowsUpdate -Install -AcceptAll -Verbose -AutoReboot
+    #Restart-Computer -Wait -Force
     Write-Output "Rebooting to complete update installation..."
     ### Schedule script execution again after reboot using Shutdown.exe
-    Shutdown /r /t 60 /c "Script will restart to check for more updates after 60 seconds."
+    #Shutdown /r /t 60 /c "Script will restart to check for more updates after 60 seconds."
   } else {
     Write-Output "No updates available."
     ### Check for existing scheduled task and remove if no updates
-    Remove-ScheduledTask -TaskName "Pre-AutoPilot-Update" -ErrorAction SilentlyContinue  
+    Write-Output "Unregistering Scheduled task"
+    Unregister-ScheduledTask -TaskName "Pre-AutoPilot-Update" -ErrorAction SilentlyContinue -Confirm:$false -Verbose
     Write-Output "Script complete. System is ready for Autopilot provisioning."
   }
 }
@@ -34,13 +35,13 @@ function CheckAndInstallUpdates {
 function CreateScheduledTask {
   ### Define scheduled task configuration
   $taskName = "Pre-AutoPilot-Update"
-  #$trigger = New-ScheduledTaskTrigger -Daily -AtStartup
-  $trigger = "-Trigger -Daily -AtStartup"
+  $trigger = New-ScheduledTaskTrigger -AtStartup
+  #$trigger = "-Trigger -Daily -AtStartup"
   $action = New-ScheduledTaskAction -Execute -Path $PSScriptPath
 
   try {
     ### Create scheduled task with elevated privileges
-    Register-ScheduledTask -TaskName $taskName $trigger -Action $action -RunLevel Highest -Force
+    Register-ScheduledTask -TaskName $taskName -Trigger $trigger -Action $action -RunLevel Highest -Force
     Write-Output "Scheduled task created to run script at startup."
   } catch {
     Write-Error "Failed to create scheduled task: ($_.Exception.Message)"
@@ -57,3 +58,4 @@ if (!$existingTask) {
 
 ### Start the update check and install process
 CheckAndInstallUpdates
+
