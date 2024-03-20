a746a22cb87c349c02f8b697ee2ba54b68b8338f
adjunctca <162944407+adjunctca@users.noreply.github.com>
2024-03-19 17:34:41 -0700

    Update updatereboot.ps1

--- updatereboot.ps1
+++ updatereboot.ps1
@@ -10,54 +10,56 @@ Install-Module -Name PSWindowsUpdate -Verbose -Force -AllowClobber -SkipPublishe
 Import-Module PSWindowsUpdate
 ### Start transcript logging
 Start-Transcript -Path "C:\scripts\updatereboot.log"
-### start time output
+### Output start time
 Write-Output $time
+
+
+### Function to create service which will run the update/reboot loop until all updates are complete ###
+function Create-UpdateService {
+    #Check if service already exists
+    if (Get-Service -Name "apautoupdate" -ErrorAction SilentlyContinue) {
+        Write-Host "Service 'apautoupdate' already exists. Proceeding!"
+    } else {
+        ###create a service to run update script on startup 
+        ###requires no user logon, therefore actually works in OOBE environment
+        sc.exe create apautoupdate binPath="C:\scripts\updatereboot.ps1" start=auto
+
+        Write-Host "Service 'apautoupdate' created successfully"
+    }
+} 
+
+### Function to remove auto update service when all updates are complete ###
+function Remove-updateServiceAndNotify {
+    # Remove the infinite update service!!!
+    sc.exe delete apautoupdate
+    Write-Host "Auto Update Service Removed..."
+    Start-Process powershell.exe -ArgumentList "-Command Write-Host 'ALL UPDATES COMPLETE - SYSTEM IS READY FOR AUTOPILOT PROVISIONING!'; Read-Host -Prompt 'Press ENTER to close'"
+}
 
 # Function to check for updates, install, and reboot if necessary
 function CheckAndInstallUpdates {
   $updateAvailable = Get-WindowsUpdate -Verbose
   if ($updateAvailable) {
     Write-Output "$time - Updates available. Installing..."
-    Get-Windowsupdate -Install -AcceptAll -AutoReboot -Verbose
+    Get-Windowsupdate -Install -AcceptAll -Verbose -AutoReboot
     Write-Output "$time - Rebooting to complete update installation..." 
+    #Restart-Computer
   } else {
     Write-Output "$time - No updates available." 
-    Write-Output "$time - Removing startup Script CMD"
-    ### Remove Startup CMD to break reboot/update loop
-    Remove-Item -Path 'C:\Users\defaultuser0\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startupdates.cmd' -ErrorAction Continue
+    Write-Output "$time - Removing service 'apautoupdate'"
+    Remove-updateServiceAndNotify
     Write-Output "$time - Script complete. System is ready for Autopilot provisioning."
   }
 }
 
-# function to add startup cmd
-function Write-StartupFile {
-    $filePath = "C:\Users\defaultuser0\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startupdates.cmd"
-    $linesToWrite = @(
-        "Powershell -Command Set-ExecutionPolicy Bypass -Force",
-        "Powershell C:\scripts\updatereboot.ps1"
-    )
 
-    ### Ensure Parents Directory already exists
-    $parentDir = Split-Path $filePath -Parent
-    if (!(Test-Path $parentDir)) {
-    New-Item -ItemType Directory -Force -Path $parentDir | Out-Null
-    }
 
-    # Create or overwrite the file
-    $null = New-Item $filePath
+### Run Auto Update service function
+Create-UpdateService
 
-    # Write lines out to file
-    $linesToWrite | Out-File -FilePath $filePath -Encoding UTF8 -Append
-}
-
-if (Test-Path "C:\Users\defaultuser0\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startupdates.cmd") {
-    Write-Output "startupdates.cmd already exists, proceeding"
-} else {
-    Write-Output "startupdates.cmd does not already exist, creating startupdates.cmd now!"
-    Write-StartupFile
-}
-
-# Start the update check and install process
+### Check for updates
+### If updates available, install and reboot ###
+### When updates complete, delete auto update service and output done message ###
 CheckAndInstallUpdates
 
 ### finish time output
